9d9f4c50b125719327d7fc1f83c315aff9f9cbfc
omiras <oscar.miras@gmail.com>
2022-06-22 08:37:08 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+//app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        nombre: "Loki",
+        edad: 1,
+        amigos: 2,
+        urls: ['https://i.picsum.photos/id/237/200/200.jpg', 'https://i.picsum.photos/id/200/200/200.jpg', 'https://i.picsum.photos/id/1062/200/200.jpg']
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+
+app.listen(3001);
