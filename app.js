aa7d410f0df862bf25c6e77e28522aae74581d0a
omiras <oscar.miras@gmail.com>
2022-06-22 16:47:20 +0200

    updated

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.set('view engine', 'ejs');
+
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
     res.render('index', {
